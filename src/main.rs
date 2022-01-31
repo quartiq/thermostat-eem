31b15ba7d2d80e18b357bc27843d43a0828c7b9b
SingularitySurfer <norman_krackow@gmx.de>
2022-01-31 15:49:36 +0100

    compiles and runs with basic rtic skeleton

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,41 @@
 extern crate panic_halt;
 pub extern crate stm32h7xx_hal;
 
-use cortex_m_rt::entry;
+// use cortex_m_rt::entry;
 use rtt_logger::RTTLogger;
 pub use stm32h7xx_hal as hal;
 
 use log::info;
 
-/// Main program function. The computer will start program execution here. duh
-#[entry]
-fn main() -> ! {
-    static LOGGER: RTTLogger = RTTLogger::new(log::LevelFilter::Trace);
-    rtt_target::rtt_init_print!();
-    log::set_logger(&LOGGER)
-        .map(|()| log::set_max_level(log::LevelFilter::Trace))
-        .unwrap();
-    info!("---Hello World");
-    loop {}
+#[rtic::app(device = hal::stm32, peripherals = true)]
+mod app{
+    use super::*;
+
+    #[shared]
+    struct Shared {
+
+    }
+
+    #[local]
+    struct Local {
+
+    }
+
+    #[init]
+    fn init(_c: init::Context) -> (Shared, Local, init::Monotonics) {
+
+        static LOGGER: RTTLogger = RTTLogger::new(log::LevelFilter::Trace);
+        rtt_target::rtt_init_print!();
+        log::set_logger(&LOGGER)
+            .map(|()| log::set_max_level(log::LevelFilter::Trace))
+            .unwrap();
+        info!("---Hello World");
+        (
+            Shared {},
+            // initial values for the `#[local]` resources
+            Local {},
+            init::Monotonics(),
+        )
+    }
+    
 }
