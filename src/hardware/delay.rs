fe2c7fe5b718456684c1176b2875e4cfc7b968b5
SingularitySurfer <norman_krackow@gmx.de>
2022-04-13 15:42:49 +0200

    cleanup custom delay, use better name and mention issue

--- src/hardware/delay.rs
+++ src/hardware/delay.rs
@@ -1,16 +1,15 @@
-use super::hal::hal::blocking::delay::{DelayMs, DelayUs};
+use super::hal::hal::blocking::delay::DelayUs;
 
 pub struct AsmDelay {
-    frequency_us: u32,
-    frequency_ms: u32,
+    cyc_per_us_corrected: u32,
 }
 
 impl AsmDelay {
     pub fn new(freq: u32) -> AsmDelay {
         AsmDelay {
-            // multiply by two to get to the frequency that halt instructions have to be issued on the STM32H7 (dual-issue pipeline)
-            frequency_us: (freq / 1_000_000) * 2,
-            frequency_ms: (freq / 1_000) * 2,
+            // Corrected value for cortex_m::asm::delay cycles per us.
+            // See https://github.com/rust-embedded/cortex-m/issues/430
+            cyc_per_us_corrected: (freq / 1_000_000) * 2,
         }
     }
 }
@@ -20,15 +19,6 @@ where
     U: Into<u32>,
 {
     fn delay_us(&mut self, us: U) {
-        cortex_m::asm::delay(self.frequency_us * us.into())
-    }
-}
-
-impl<U> DelayMs<U> for AsmDelay
-where
-    U: Into<u32>,
-{
-    fn delay_ms(&mut self, ms: U) {
-        cortex_m::asm::delay(self.frequency_ms * ms.into())
+        cortex_m::asm::delay(self.cyc_per_us_corrected * us.into())
     }
 }
