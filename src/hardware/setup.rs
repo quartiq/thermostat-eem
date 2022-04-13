e201233c88f184c0b959941e1a253af7ce65996a
SingularitySurfer <norman_krackow@gmx.de>
2022-04-13 17:48:38 +0200

    add note about race condition in interrupt enable

--- src/hardware/setup.rs
+++ src/hardware/setup.rs
@@ -319,6 +319,8 @@ pub fn setup(
         },
     );
     // Enable interrupt after all ADC setup is done.
+    // *Note*: Race condition: If the first ADC already sampled more than once by this point 
+    // the interrupt readout sequence breaks.
     adc.rdyn.enable_interrupt(&mut exti);
 
     info!("Setup Ethernet");
