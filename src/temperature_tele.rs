6c2bf2ad8163cd4c245789817808fad1f1d65986
SingularitySurfer <norman_krackow@gmx.de>
2022-05-20 14:58:08 +0200

    min/max infinity reset and mean instead of average

--- src/temperature_tele.rs
+++ src/temperature_tele.rs
@@ -5,12 +5,12 @@ use serde::Serialize;
 /// Temperature telemetry struct.
 #[derive(Serialize, Copy, Clone, Default, Debug)]
 pub struct Temperature {
-    average: f32,
+    mean: f32,
     min: f32,
     max: f32,
 }
 
-/// Temperature buffer for computing min/max/average of the last telemetry period.
+/// Temperature buffer for computing min/max/mean of the last telemetry period.
 #[derive(Default, Copy, Clone, Debug)]
 pub struct TemperatureBuffer {
     min: f32,
@@ -23,30 +23,25 @@ impl TemperatureBuffer {
     /// Add a new temperature sample to the buffer. This will add it to the accumulator,
     /// update min/max and increment the counter.
     pub fn add(&mut self, temp: f32) {
-        if self.counter == 0 {
-            self.min = temp;
-            self.max = temp;
-        } else {
-            self.max = self.max.max(temp);
-            self.min = self.min.min(temp);
-        }
+        self.max = self.max.max(temp);
+        self.min = self.min.min(temp);
         self.accumulator += temp;
         self.counter += 1;
     }
 
-    /// Process temperature buffer. Calculates average, returns the finalized Temperature type
+    /// Process temperature buffer. Calculates mean, returns the finalized Temperature type
     /// and resets the buffer.
     pub fn process(&mut self) -> Temperature {
-        let average = self.accumulator / self.counter as f32;
+        let mean = self.accumulator / self.counter as f32;
         let temp = Temperature {
-            average,
+            mean,
             max: self.max,
             min: self.min,
         };
         self.counter = 0;
         self.accumulator = 0.;
-        self.max = 0.;
-        self.min = 0.;
+        self.max = f32::NEG_INFINITY;
+        self.min = f32::INFINITY;
         temp
     }
 }
