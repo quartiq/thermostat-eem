6c9fb8794b95840e5239f0c6e6e8aebec0a7b1da
SingularitySurfer <norman_krackow@gmx.de>
2022-08-26 12:18:19 +0200

    cleanup and prep for PR

---
+++ py/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Setup script for Thermostat EEM
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="thermostat",
+    packages=find_packages(),
+    # Keep versions in Cargo.toml and py/setup.py synchronized.
+    version="0.1.0",
+    description="Thermostat Utilities",
+    author="QUARTIQ GmbH",
+    license="MIT",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "gmqtt",
+        # pylint: disable=line-too-long
+        "miniconf-mqtt@git+https://github.com/quartiq/miniconf@develop#subdirectory=py/miniconf-mqtt",
+    ],
+)
