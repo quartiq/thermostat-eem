3ccfcae9bc793d905cffa6cff4d264947f518e91
Robert Jördens <rj@quartiq.de>
2024-02-07 17:43:29 +0000

    better statistics

--- src/statistics.rs
+++ src/statistics.rs
@@ -6,6 +6,8 @@ use serde::Serialize;
 #[derive(Serialize, Copy, Clone, Default, Debug)]
 pub struct Statistics {
     mean: f32,
+    ptp: f32,
+    std: f32,
 }
 
 impl From<Buffer> for Option<Statistics> {
@@ -13,8 +15,14 @@ impl From<Buffer> for Option<Statistics> {
     /// and resets the buffer.
     fn from(buff: Buffer) -> Self {
         if buff.counter > 0 {
-            let mean = buff.accumulator / buff.counter as f64;
-            Some(Statistics { mean: mean as f32 })
+            let c = 1.0 / buff.counter as f64;
+            let mean = buff.m1 * c;
+            let var = buff.m2 * c - mean * mean;
+            Some(Statistics {
+                mean: (mean + buff.first) as f32,
+                ptp: (buff.max - buff.min) as f32,
+                std: libm::sqrtf(var as f32),
+            })
         } else {
             None
         }
@@ -24,9 +32,11 @@ impl From<Buffer> for Option<Statistics> {
 /// Statistics buffer for computing min/max/mean of the last telemetry period.
 #[derive(Copy, Clone, Debug)]
 pub struct Buffer {
-    min: f32,
-    max: f32,
-    accumulator: f64,
+    min: f64,
+    max: f64,
+    m1: f64,
+    m2: f64,
+    first: f64,
     counter: u32,
 }
 
@@ -34,9 +44,14 @@ impl Buffer {
     /// Add a new temperature sample to the buffer. This will add it to the accumulator,
     /// update min/max and increment the counter.
     pub fn update(&mut self, temp: f64) {
-        self.max = self.max.max(temp as f32);
-        self.min = self.min.min(temp as f32);
-        self.accumulator += temp;
+        self.max = self.max.max(temp);
+        self.min = self.min.min(temp);
+        if self.counter == 0 {
+            self.first = temp;
+        };
+        let t = temp - self.first;
+        self.m1 += t;
+        self.m2 += t * t;
         self.counter += 1;
     }
 }
@@ -45,9 +60,11 @@ impl Default for Buffer {
     fn default() -> Self {
         Self {
             counter: 0,
-            accumulator: 0.,
-            max: f32::NEG_INFINITY,
-            min: f32::INFINITY,
+            m1: 0.,
+            m2: 0.,
+            first: 0.,
+            max: f64::NEG_INFINITY,
+            min: f64::INFINITY,
         }
     }
 }
