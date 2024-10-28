62428f9b0969b4a9fdab1f487f6db4dd5b4feba6
Robert Jördens <rj@quartiq.de>
2024-10-28 12:40:16 +0100

    remove sample period constant from py

--- py/thermostat/__init__.py
+++ py/thermostat/__init__.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
 """Thermostat EEM utilities"""
-
-# Sample period in seconds for all channels.
-SAMPLE_PERIOD = 1 / 1007  # ADC ODR 1007 split between ADC channels. But with the ZOH the IIR updates at full rate.
